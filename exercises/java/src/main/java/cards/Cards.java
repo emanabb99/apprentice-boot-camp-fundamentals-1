83afe15b2be6524a59f30f1724f41b6f560c8193
Eman Abbas <eman.abbas@autotrader.co.uk>
2026-05-06 10:21:11 +0100

    Added hello world

--- exercises/java/src/main/java/cards/Cards.java
+++ exercises/java/src/main/java/cards/Cards.java
@@ -2,6 +2,7 @@ package cards;
 
 public class Cards {
     public static void main(String[] args) {
+        System.out.println("Hello world");
         Cards cards = new Cards();
         String[] deckInOrder = cards.getCards();
         for(String card: deckInOrder){
